8454c85c2763722a0b8c71fb5657d02b1c75d43a
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-07-25 17:15:22 +0100

    Add walk animation and transition between idle and walk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 10f;
 
+    private bool isWalking;
+
     private void Update(){
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W)) {
@@ -26,9 +28,14 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
-        Debug.Log(transform.forward);
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
