8d1d1d0525c7557e8f501eb173139f87dc500f1c
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-07-28 11:50:49 +0100

    Prefab for clear counter + collision with player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,8 +8,33 @@ public class Player : MonoBehaviour {
     [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     private void Update(){
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+    private void HandleInteractions() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance)) {
+            Debug.Log(raycastHit.transform);
+        } else {
+            Debug.Log("-");
+        }
+    }
+    private void HandleMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -28,14 +53,16 @@ public class Player : MonoBehaviour {
             if (canMove) {
                 // Can move only on the X
                 moveDir = moveDirX;
-            } else {
+            }
+            else {
                 // Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove) {
                     moveDir = moveDirZ;
-                } else {
+                }
+                else {
                     // cannot move in any direction
                 }
             }
@@ -43,15 +70,11 @@ public class Player : MonoBehaviour {
         if (canMove) {
             transform.position += moveDir * moveDistance;
         }
-        
+
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking() {
-        return isWalking;
     }
 
 }
