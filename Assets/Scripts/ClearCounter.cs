3d9eb8043d474f4501de2f19f6ff6778b41c9559
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-07-31 21:16:23 +0100

    ScriptableObjects for spawning items and also knowing the item being spawned

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour {
 
     // It's personal preference whether to use type 'Transform' or 'GameObject' for when declaring a prefab
-    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
 
     public void Interact() {
         Debug.Log("interact");
-        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
