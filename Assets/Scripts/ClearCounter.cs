6d6dfcb307e8579d0f938017153c20b92c9e00df
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-07-31 20:54:27 +0100

    Create Empty Game Object to spawn tomato on top of counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
-{
+public class ClearCounter : MonoBehaviour {
+
+    // It's personal preference whether to use type 'Transform' or 'GameObject' for when declaring a prefab
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact() {
         Debug.Log("interact");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
