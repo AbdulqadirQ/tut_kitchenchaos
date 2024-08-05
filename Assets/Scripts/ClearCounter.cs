dd7df5bd4912b2728fe4d74c51ce1c818ddbed2b
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-08-05 21:52:43 +0100

    Now using the well-thought-out code, some simple logic added for picking up objects and dropping them

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,6 +7,23 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     
     public override void Interact(Player player) {
-
+        if (!HasKitchenObject()) {
+            // There's no kitchen object here
+            if (player.HasKitchenObject()) {
+                // Player is carrying a kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else {
+                // Player is not carrying anything so do nothing
+            }
+        } else {
+            // There's a kitchen object here
+            if (player.HasKitchenObject()) {
+                // Player is carrying a kitchen object so do nothing
+            } else {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
