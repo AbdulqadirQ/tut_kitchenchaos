cf7c616d253649e016bea08f2f54c85d399de00d
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-08-05 20:55:38 +0100

    Refactor with BaseContainer class. Also remove some logic so object goes straight into player's hand

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCounter : MonoBehaviour {
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent {
+
+    // It's personal preference whether to use type 'Transform' or 'GameObject' for when declaring a prefab
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
 
     // the 'protected' accessor is visible to this class and any class inheriting it
     // 'virtual' allows the child class to implement in their own way
     // 'abstract' is an alternative to 'virtual' which forces all child classes to have their own implementation
     public virtual void Interact(Player player) {
         Debug.LogError("BaseCounter.Interact(); entered which shouldn't happen");
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
