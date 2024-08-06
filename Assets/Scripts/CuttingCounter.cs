b78edfdb9ea1386b0d6227715a450ab8484c397e
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-08-06 22:44:12 +0100

    Validation to only cut what can be cut, in this case it's whatever isn't used as an input recipe

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -11,7 +11,11 @@ public class CuttingCounter : BaseCounter {
             // There's no kitchen object here
             if (player.HasKitchenObject()) {
                 // Player is carrying a kitchen object
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO())) {
+                    // Player is carrying something that can be cut
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
+                
             }
             else {
                 // Player is not carrying anything so do nothing
@@ -29,13 +33,22 @@ public class CuttingCounter : BaseCounter {
         }
     }
     public override void InteractAlternate(Player player) {
-        if (HasKitchenObject()) {
-            // There is a KitchenObject here
+        if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO())) {
+            // There is a KitchenObject here AND it can be cut
             KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
             GetKitchenObject().DestroySelf();
 
             KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO) {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray) {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO) {
