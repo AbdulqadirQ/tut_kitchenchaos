b8e6e42f0fa10cacccd66cfeea838a0589c918de
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-08-07 18:59:31 +0100

    Show and Hide progress bar when needed

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -12,9 +12,25 @@ public class ProgressBarUI : MonoBehaviour {
         cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
 
         barImage.fillAmount = 0;
+
+        Hide();
     }
 
     private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgresChangedEventArgs e) {
         barImage.fillAmount = e.progressNormalized;
+
+        if (e.progressNormalized == 0f || e.progressNormalized == 1f) {
+            Hide();
+        } else {
+            Show();
+        }
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
     }
 }
