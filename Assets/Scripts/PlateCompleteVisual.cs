51da1602bd3486f157f9a55cbca600421eb4c34f
AbdulqadirQ <a.q.r@hotmail.co.uk>
2024-08-11 12:01:54 +0100

    Struct to define custom KitchenObejctSO/GameObject type to build-up a visual of the plate on event of adding that ingrediant

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour {
+
+    // For storing data without any logic, it makes more sense to use a Struct instead of a Class
+    // `Serializable` allows our custom type to be available within Unity, to then add the list of plate components (cheese, bread etc.)
+    [Serializable]
+    public struct KitchenObjectSO_GameObject {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSOGameObjectList;
+
+    private void Start() {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList) {
+            kitchenObjectSOGameObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e) {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList) {
+            if (kitchenObjectSOGameObject.kitchenObjectSO == e.kitchenObjectSO) {
+                kitchenObjectSOGameObject.gameObject.SetActive(true);
+            }
+        }
+    }
+}
